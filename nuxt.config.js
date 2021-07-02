23627b60a4e149e23a58a2895a40d32051f74f79
alageshdev-2019 <alageshdev2019@gmail.com>
2021-07-02 13:10:28 +0800

    change port

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ import colors from 'vuetify/es5/util/colors'
 export default {
  mode:"universal",
  server:{
-    port:3022
+    port:3000
  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
